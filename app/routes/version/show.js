7de75706175882816b1ca0cd17cae36378dfcd6b
Robert Wagner <rwwagner90@gmail.com>
2018-01-21 11:20:13 -0500

    Add 404 page
    This should redirect to 404 whenever the guides data returns a 404. I initially wanted to make it a wildcard route, but that didn't work because of the way the app is setup, so I think this is the most correct way to handle things.

--- app/routes/version/show.js
+++ app/routes/version/show.js
@@ -1,36 +1,29 @@
 import Route from '@ember/routing/route';
-import { get } from '@ember/object';
 import { next } from '@ember/runloop';
 
 export default Route.extend({
   model(params) {
     let versionModel = this.modelFor('version');
 
+    const { version } = versionModel;
+    const path = params.path.replace(/\/$/, '');
+
     return this.store.queryRecord('content', {
-      path: params.path.replace(/\/$/, ''),
-      version: versionModel.version,
+      path,
+      version
     })
-    .catch((e) => {
-      if (get(e, 'errors.0.status') === "404") {
-        return this.store.queryRecord('content', {
-          path: `${params.path.replace(/\/$/, '')}/index`,
-          version: versionModel.version,
-        });
-      }
-      throw e;
-    });
   },
 
   afterModel() {
     next(this, () => {
       Prism.highlightAll();
-    })
+    });
   },
 
   actions: {
     didTransition() {
       this._super();
-      window.scrollTo(0,0);
+      window.scrollTo(0, 0);
     }
   }
 });
