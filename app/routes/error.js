08c2b3bdc722e4481bae1eb8789cb58459fc3a19
Robert Wagner <rwwagner90@gmail.com>
2018-01-23 20:34:17 -0500

    Start on page titles

--- app/routes/error.js
+++ app/routes/error.js
@@ -1,5 +1,22 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { get, setProperties } from '@ember/object';
 
 export default Route.extend({
-  classNames: ["x404"]
-})
+  headData: service(),
+
+  classNames: ['x404'],
+
+  setupController(controller, error) {
+    const errorCode = get(error, 'errors.0.status');
+    let errorMessage = 'Error';
+
+    if (errorCode === '404') {
+      errorMessage = '404 - Not Found';
+    }
+
+    return setProperties(this.get('headData'), {
+      title: `Ember.js - ${errorMessage}`
+    });
+  }
+});
