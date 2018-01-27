cfca7da6c0107426ccd822fe8c5f7aa1a9023a59
Jen Weber <j@jenweber.me>
2018-01-27 11:55:43 -0500

    implement current version 🎉 #30

--- app/routes/version.js
+++ app/routes/version.js
@@ -5,11 +5,16 @@ import { get } from '@ember/object';
 export default Route.extend({
   model(params) {
     let applicationModel = this.modelFor('application');
-    
+    let version;
+    if (params.version === 'current') {
+      version = get(applicationModel, 'currentVersion');
+    } else {
+      version = params.version;
+    }
     return hash({
-      pages: this.store.query('page', {version: params.version}),
+      pages: this.store.query('page', {version}),
       allVersions: get(applicationModel, 'allVersions'),
-      version: params.version,
+      version: version,
     });
   }
 });
