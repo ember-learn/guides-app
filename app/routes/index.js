ac6f71c4315ba0ed40df293bb01badc35dda97e8
Chris Manson <chris@stonecircle.io>
2018-05-02 08:42:21 +0100

    redirect to /release properly

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,7 @@
 import Route from '@ember/routing/route';
-import { get } from '@ember/object';
 
 export default Route.extend({
   redirect() {
-    this.transitionTo('version', get(this.modelFor('application'), 'currentVersion'));
+    this.transitionTo('version', 'release');
   }
 });
