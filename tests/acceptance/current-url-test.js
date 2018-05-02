29944974e79e3134897f39c305e7831789c0fdb4
Chris Manson <chris@stonecircle.io>
2018-05-02 10:49:05 +0100

    adding acceptance test for / => /release redirect

--- tests/acceptance/current-url-test.js
+++ tests/acceptance/current-url-test.js
@@ -23,3 +23,14 @@ test('visiting /release-url', function (assert) {
     assert.equal(find('.ember-basic-dropdown-trigger').text().trim(), currentVersion);
   });
 });
+
+test('visiting / redirects you to /release', function (assert) {
+  visit('/');
+  let page = this.application.__container__.lookup('service:page');
+  andThen(function () {
+    assert.equal(currentURL(), "/release");
+
+    let currentVersion = page.get('currentVersion');
+    assert.equal(find('.ember-basic-dropdown-trigger').text().trim(), currentVersion);
+  });
+});
