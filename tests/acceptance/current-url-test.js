b11671ff65feb94a797381826d0f66d482854503
Sivakumar Kailasam <siva@rubberduck.engineering>
2018-05-04 22:12:33 +0530

    Rewrite current urls in proxy

--- tests/acceptance/current-url-test.js
+++ tests/acceptance/current-url-test.js
@@ -5,19 +5,6 @@ import { module, test } from 'qunit';
 module('Acceptance | current url', function(hooks) {
   setupApplicationTest(hooks);
 
-  /**
-   * In the old Guides app, `/current` used to redirect to the latest
-   * version of the guides. So to honor old bookmarks we still
-   * preserve this behavior while introducing `/release` path to be
-   * consistent with ember API docs app.
-  */
-  test('visiting /current-url', async function(assert) {
-    await visit('/current');
-    let page = this.owner.lookup('service:page');
-    let currentVersion = page.get('currentVersion');
-    assert.equal(find('.ember-basic-dropdown-trigger').textContent.trim(), currentVersion);
-  });
-
   test('visiting /release-url', async function(assert) {
     await visit('/release');
     let page = this.owner.lookup('service:page');
