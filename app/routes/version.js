34b4ee1d3baf015ac530d50be5124dc6bd642034
Sivakumar Kailasam <siva@rubberduck.engineering>
2018-05-05 11:24:13 +0530

    Use nginx to match current app's /current behavior

--- app/routes/version.js
+++ app/routes/version.js
@@ -1,21 +1,17 @@
-import Route from '@ember/routing/route';
-import { hash } from 'rsvp';
-import { inject as service } from '@ember/service';
 import { get, set } from '@ember/object';
+import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
+import { hash } from 'rsvp';
 
 export default Route.extend({
   page: service(),
   model(params) {
     let applicationModel = this.modelFor('application');
     let currentVersion = get(applicationModel, 'currentVersion');
-    let version;
+    let version = params.version;
 
-    // We need to support legacy `current` urls, but we should use release
-    // in order to keep in step with ember-api-docs url convention
-    if (params.version === 'current' || params.version === 'release') {
+    if (params.version === 'release') {
       version = currentVersion;
-    } else {
-      version = params.version;
     }
 
     return hash({
--- tests/acceptance/current-url-test.js
+++ tests/acceptance/current-url-test.js
@@ -1,19 +1,7 @@
-import { test } from 'qunit';
 import moduleForAcceptance from 'guides-app/tests/helpers/module-for-acceptance';
+import { test } from 'qunit';
 
 moduleForAcceptance('Acceptance | current url');
-
-// We need to support legacy `current` urls, but we should use release
-// in order to keep in step with ember-api-docs url convention
-
-test('visiting /current-url', function(assert) {
-  visit('/current');
-  let page = this.application.__container__.lookup('service:page');
-  andThen(function() {
-    let currentVersion = page.get('currentVersion');
-    assert.equal(find('.ember-basic-dropdown-trigger').text().trim(), currentVersion);
-  });
-});
 
 test('visiting /release-url', function (assert) {
   visit('/release');
