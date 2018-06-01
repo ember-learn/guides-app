062127b04361567cf4eb145978f63e875edbaf76
Chris Manson <chris@stonecircle.io>
2018-06-01 11:04:30 +0100

    first pass on adding new meta

--- app/routes/error.js
+++ app/routes/error.js
@@ -1,22 +1,5 @@
 import Route from '@ember/routing/route';
-import { inject as service } from '@ember/service';
-import { get, setProperties } from '@ember/object';
 
 export default Route.extend({
-  headData: service(),
-
   classNames: ['x404'],
-
-  setupController(controller, error) {
-    const errorCode = get(error, 'errors.0.status');
-    let errorMessage = 'Error';
-
-    if (errorCode === '404') {
-      errorMessage = '404 - Not Found';
-    }
-
-    return setProperties(this.headData, {
-      title: `Ember.js - ${errorMessage}`
-    });
-  }
 });
