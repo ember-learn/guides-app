1f2b44dee3b854159f818c735a95351e6de19a8b
Chris Manson <chris@stonecircle.io>
2018-06-01 11:43:05 +0100

    allow the ability to override description and canonical

--- app/models/content.js
+++ app/models/content.js
@@ -1,6 +1,8 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  title: DS.attr('string'),
-  content: DS.attr('string'),
+  title: DS.attr(),
+  content: DS.attr(),
+  description: DS.attr(),
+  canonical: DS.attr(),
 });
