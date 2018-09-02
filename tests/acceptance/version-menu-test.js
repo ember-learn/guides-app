244ab3a8f850c3f72dcb29640d5b3bb6eb14995f
Edward Robinson <proteenx11@gmail.com>
2018-09-03 08:47:55 +1200

    take advantage of powerSelect helper

--- tests/acceptance/version-menu-test.js
+++ tests/acceptance/version-menu-test.js
@@ -1,39 +1,32 @@
-import { click, currentURL, visit } from '@ember/test-helpers';
+import { currentURL, visit } from '@ember/test-helpers';
 import { module, test } from 'qunit';
 import { setupApplicationTest } from 'ember-qunit';
-
-function selectOption(option) {
-  return [].find.call(document.querySelectorAll('.ember-power-select-option'), (element) => element.innerText === option)
-}
+import { selectChoose } from 'ember-power-select/test-support';
 
 module('Acceptance | version menu when changing versions', function(hooks) {
   setupApplicationTest(hooks);
 
   test('stays on index page', async function(assert) {
     await visit('/v2.17.0/');
-    await click('.ember-basic-dropdown-trigger')
-    await click(selectOption("2.10"))
+    await selectChoose('.ember-basic-dropdown-trigger', '2.10');
     assert.equal(currentURL(), '/v2.10.0');
   });
 
   test('stays on same section', async function(assert) {
     await visit('/v1.13.0/getting-started/');
-    await click('.ember-basic-dropdown-trigger')
-    await click(selectOption("1.12"))
+    await selectChoose('.ember-basic-dropdown-trigger', '1.12');
     assert.equal(currentURL(), '/v1.12.0/getting-started');
   });
 
   test('stays on same section and page', async function(assert) {
     await visit('/v3.0.0/object-model/classes-and-instances/');
-    await click('.ember-basic-dropdown-trigger')
-    await click(selectOption("3.1"))
+    await selectChoose('.ember-basic-dropdown-trigger', '3.1');
     assert.equal(currentURL(), '/v3.1.0/object-model/classes-and-instances');
   });
 
   test("redirects to index page if current section/page doesn't exist in new version", async function(assert) {
     await visit('/v1.10.0/getting-started/using-fixtures/');
-    await click('.ember-basic-dropdown-trigger')
-    await click(selectOption("1.13"))
+    await selectChoose('.ember-basic-dropdown-trigger', '1.13');
     assert.equal(currentURL(), '/v1.13.0');
   });
 });
