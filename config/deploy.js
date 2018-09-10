9d38431c1109d83d39dde771665bedf73f4f4cdb
Chris Manson <chris.manson@simplabs.com>
2018-09-10 08:24:48 +0100

    starting to implement ember-cli-deploy for algolia indexing

---
+++ config/deploy.js
@@ -0,0 +1,37 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = function(deployTarget) {
+  let ENV = {
+    build: {},
+    'prember-algolia': {
+      indexName: 'ember-guides',
+      applicationId: 'EUXSOLYEM4',
+      apiKey: 'get-from-env',
+      tagsToExclude: '.old-version-warning,.edit-page,code,pre',
+      versionsToIgnore: ['1.x'],
+      versionPattern: /^(v\d+\.\d+\.\d+)\//,
+      pathPattern: /^v\d+\.\d+\.\d+\/(.*)\/index.html$/,
+    }
+  };
+
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'development';
+    // configure other plugins for development deploy target here
+  }
+
+  if (deployTarget === 'staging') {
+    ENV.build.environment = 'production';
+    // configure other plugins for staging deploy target here
+  }
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+    // configure other plugins for production deploy target here
+  }
+
+  // Note: if you need to build some configuration asynchronously, you can return
+  // a promise that resolves with the ENV object instead of returning the
+  // ENV object synchronously.
+  return ENV;
+};
